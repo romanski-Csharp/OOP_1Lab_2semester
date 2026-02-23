d048f86edc107e013ff21b8fe769bc58794528a6
Роман Чуб <Tonik7star@gmail.com>
2026-02-23 17:55:42 +0200

    Add 3 Task solution

--- OOP_1Lab_3Task/Program.cs
+++ OOP_1Lab_3Task/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        double eps = 0.0001;
+
+        double result1 = Service.CalculateSum(n => 1.0 / Math.Pow(2, n), eps);
+        Console.WriteLine($"Сума ряду 1 / 2^n: {result1}");
+
+        double result2 = Service.CalculateSum(n => 1 / Service.Factorial(n + 1), eps);
+        Console.WriteLine($"Сума ряду 1 / (n + 1)!: {result2}");
+
+        double result3 = Service.CalculateSum(n => Math.Pow(-1, n + 1) / Math.Pow(2, n), eps);
+        Console.WriteLine($"Сума ряду -1^(n + 1) / 2^n: {result3}");
+    }
+}
---
+++ OOP_1Lab_3Task/Service.cs
@@ -0,0 +1,26 @@
+﻿static class Service
+{
+    public static double CalculateSum(Func<int, double> getTerm, double epsilon)
+    {
+        double sum = 0;
+        double currentTerm;
+        int n = 0; 
+
+        do
+        {
+            currentTerm = getTerm(n); 
+            sum += currentTerm;
+            n++;
+        }
+        while (Math.Abs(currentTerm) >= epsilon);
+
+        return sum;
+    }
+    public static double Factorial(int n)
+    {
+        double res = 1;
+        for (int i = 1; i <= n; i++) res *= i;
+        return res;
+    }
+}
+
